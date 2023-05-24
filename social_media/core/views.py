ab6c70c90ffb7c67a8e9a84f0fc1a029b41590b4
Mohammad Al Rafi <arafi304@gmail.com>
2023-05-24 21:47:17 +0600

    Posting functionality done, displaying posts done

--- social_media/core/views.py
+++ social_media/core/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .models import Post
 
 def home(request):
-    if request.user.is_authenticated:
-        return render(request, 'core/home.html')
-    else:
+    if not request.user.is_authenticated:
         return redirect('login')
+    else:
+        posts = Post.objects.all().order_by('-posted_at')
+
+        if request.method == 'POST':
+            post_text = request.POST['post-text']
+            post = Post()
+            post.text = post_text
+            post.account = request.user
+            post.save()
+
+            messages.success(request, 'Your post has been submitted!')
+
+        return render(request, 'core/home.html', {
+            'posts': posts,
+        })
