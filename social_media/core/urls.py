74e76aa48be6b98a3827142aa7d3723b2fb74484
Mohammad Al Rafi <arafi304@gmail.com>
2023-05-25 23:10:49 +0600

    Commenting functionality Done, UI tweaks, posting method changed

--- social_media/core/urls.py
+++ social_media/core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name ='home'),
+    path('post', views.post, name='post'),
+    path('<int:post_id>/comment', views.comment, name='comment'),
     
 ]
--- social_media/core/views.py
+++ social_media/core/views.py
@@ -1,23 +1,45 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from .models import Post
+from .models import Post, Comment, Follow, Like
 
 def home(request):
     if not request.user.is_authenticated:
         return redirect('login')
     else:
         posts = Post.objects.all().order_by('-posted_at')
+        post_element = []
 
+        for post in posts:
+            comments = Comment.objects.filter(post = post).order_by('-commented_at')
+            temp = {
+                'post': post,
+                'comments': comments if len(comments) > 0 else None,
+            }
+            post_element.append(temp)
+
+        return render(request, 'core/home.html', {
+            'post_element': post_element,
+        })
+
+def post(request):
         if request.method == 'POST':
             post_text = request.POST['post-text']
             post = Post()
             post.text = post_text
             post.account = request.user
             post.save()
-
             messages.success(request, 'Your post has been submitted!')
 
-        return render(request, 'core/home.html', {
-            'posts': posts,
-        })
+        return redirect('home')
+
+def comment(request, post_id):
+    if request.method == "POST":
+        comment_text = request.POST['comment-text']
+
+        comment = Comment()
+        comment.account = request.user
+        comment.post = Post.objects.get(id=post_id)
+        comment.comment = comment_text
+        comment.save()
+    return redirect('home')
