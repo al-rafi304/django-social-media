85759e246b266999b3a366d7e41a310399fe462b
Mohammad Al Rafi <arafi304@gmail.com>
2023-05-26 14:36:55 +0600

    Like/unlike feature done, UI tweaks

--- social_media/core/views.py
+++ social_media/core/views.py
@@ -12,12 +12,13 @@ def home(request):
 
         for post in posts:
             comments = Comment.objects.filter(post = post).order_by('-commented_at')
+            liked = True if Like.objects.filter(post=post, account=request.user).exists() else False
             temp = {
                 'post': post,
                 'comments': comments if len(comments) > 0 else None,
+                'liked': liked
             }
             post_element.append(temp)
-
         return render(request, 'core/home.html', {
             'post_element': post_element,
         })
@@ -35,11 +36,32 @@ def post(request):
 
 def comment(request, post_id):
     if request.method == "POST":
+        post = Post.objects.get(id=post_id)
+
         comment_text = request.POST['comment-text']
 
         comment = Comment()
         comment.account = request.user
-        comment.post = Post.objects.get(id=post_id)
+        comment.post = post
         comment.comment = comment_text
         comment.save()
+
+        post.commentCount += 1
+    return redirect('home')
+
+def like(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if Like.objects.filter(post=post_id, account=request.user).exists() == False:       # Have to use filter instead of get to use exists()
+        like = Like()
+        like.account = request.user
+        like.post = post
+        like.save()
+
+        post.likeCount += 1
+    else:
+        like = Like.objects.get(post=post_id, account=request.user)
+        like.delete()
+        post.likeCount -= 1
+    
+    post.save()
     return redirect('home')
